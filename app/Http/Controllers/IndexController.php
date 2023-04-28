c100f220110b277850aaff202630d4f3e75405d5
raduubraduu <lefter_radu@yahoo.com>
2023-04-28 16:45:10 +0100

    linked pages using inertia

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -8,11 +8,11 @@ class IndexController extends Controller
 {
     public function index()
     {
-        return 'Index';
+        return inertia('Index/Index');
     }
 
     public function show()
     {
-        return 'Show';
+        return inertia('Index/Show');
     }
 }
