dd88f8b0b30699a38b7490a9ec470039a58df4ca
raduubraduu <lefter_radu@yahoo.com>
2023-04-29 16:56:31 +0100

    handled form submission

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -32,7 +32,10 @@ class ListingController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        Listing::create($request->all());
+
+        return redirect()->route('listing.index')
+            ->with('success', 'Listing was created!');
     }
 
     /**
