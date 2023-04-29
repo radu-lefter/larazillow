6c2100601c16c03e2c7ce985b2e47ff24cf492ec
raduubraduu <lefter_radu@yahoo.com>
2023-04-29 17:55:37 +0100

    impl edit funct for listings

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,15 +1,18 @@
 <template>
-    <div v-for="listing in listings" :key="listing.id">
-      <Link :href="`/listing/${listing.id}`">
-        <ListingAddress :listing="listing" />
-      </Link>
+  <div v-for="listing in listings" :key="listing.id">
+    <Link :href="`/listing/${listing.id}`">
+    <ListingAddress :listing="listing" />
+    </Link>
+    <div>
+      <Link :href="`/listing/${listing.id}/edit`">Edit</Link>
     </div>
-  </template>
+  </div>
+</template>
   
-  <script setup>
-  import {Link} from '@inertiajs/vue3'
-  import ListingAddress from '@/Components/ListingAddress.vue'
-  defineProps({
-    listings: Array,
-  })
-  </script>
+<script setup>
+import { Link } from '@inertiajs/vue3'
+import ListingAddress from '@/Components/ListingAddress.vue'
+defineProps({
+  listings: Array,
+})
+</script>
