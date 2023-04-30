a6e099a2a504af9f80dec15f1bd9ad9089744e3a
raduubraduu <lefter_radu@yahoo.com>
2023-04-30 22:27:46 +0100

    created registration functionality

--- app/Http/Controllers/UserAccountController.php
+++ app/Http/Controllers/UserAccountController.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 use Illuminate\Http\Request;
 
@@ -9,5 +12,20 @@ class UserAccountController extends Controller
     public function create()
     {
         return inertia('UserAccount/Create');
+    }
+
+    public function store(Request $request)
+    {
+        $user = User::make($request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8|confirmed'
+        ]));
+        $user->password = Hash::make($user->password);
+        $user->save();
+        Auth::login($user);
+
+        return redirect()->route('listing.index')
+            ->with('success', 'Account created!');
     }
 }
--- resources/js/Pages/UserAccount/Create.vue
+++ resources/js/Pages/UserAccount/Create.vue
@@ -63,5 +63,5 @@
     password: null,
     password_confirmation: null,
   })
-  const register = () => form.post('')
+  const register = () => form.post(route('user-account.store'))
   </script>
