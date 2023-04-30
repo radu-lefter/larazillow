dff48482bc150e5b1f285623bcbf0b10516d14ae
raduubraduu <lefter_radu@yahoo.com>
2023-04-30 22:39:54 +0100

    added links from login to register and viceversa

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,12 +6,21 @@ use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Hash;
+
 class IndexController extends Controller
 {
     public function index()
     {
         //dd(Auth::user());
         //dd(Auth::check());
+
+        // dd(
+        //     Hash::make('password'),
+        //     '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+        //     Hash::check('password', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi')
+        // );
+
         return inertia(
             'Index/Index',
             [
