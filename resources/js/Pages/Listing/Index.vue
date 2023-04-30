a15c3e8c7b3545f7f9117cbc2e51d2c687b7251d
raduubraduu <lefter_radu@yahoo.com>
2023-04-30 17:26:22 +0100

    created new component for listing space details

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -2,25 +2,19 @@
   <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
     <Box v-for="listing in listings" :key="listing.id">
       <div>
-        <Link
-          :href="route('listing.show', {listing: listing.id})"
-        >
-          <ListingAddress :listing="listing" />
+        <Link :href="route('listing.show', { listing: listing.id })">
+        <ListingSpace :listing="listing" class="text-lg" />
+        <ListingAddress :listing="listing" class="text-gray-500" />
         </Link>
       </div>
       <div>
-        <Link
-          :href="route('listing.edit', {listing: listing.id})"
-        >
-          Edit
+        <Link :href="route('listing.edit', { listing: listing.id })">
+        Edit
         </Link>
       </div>
       <div>
-        <Link
-          :href="route('listing.destroy', {listing: listing.id})"
-          method="DELETE" as="button"
-        >
-          Delete
+        <Link :href="route('listing.destroy', { listing: listing.id })" method="DELETE" as="button">
+        Delete
         </Link>
       </div>
     </Box>
@@ -30,6 +24,7 @@
 <script setup>
 import { Link } from '@inertiajs/vue3'
 import ListingAddress from '@/Components/ListingAddress.vue'
+import ListingSpace from '@/Components/ListingSpace.vue'
 import Box from '@/Components/UI/Box.vue'
 defineProps({
   listings: Array,
