e7600ec332e62abbaead62660cc4ad401844a6ec
raduubraduu <lefter_radu@yahoo.com>
2023-05-01 22:37:29 +0100

    created realtor controller and grouped routes

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,10 @@ Route::delete('logout', [AuthController::class, 'destroy'])
 
 Route::resource('user-account', UserAccountController::class)
   ->only(['create', 'store']);
+
+Route::prefix('realtor')
+  ->name('realtor.')
+  ->middleware('auth')
+  ->group(function () {
+    Route::resource('listing', RealtorListingController::class);
+  });
