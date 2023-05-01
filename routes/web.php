279e1c71ef135037245f4824f72368615515cd47
raduubraduu <lefter_radu@yahoo.com>
2023-05-01 22:48:49 +0100

    created func to show realtor listings

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserAccountController;
+use App\Http\Controllers\RealtorListingController;
 
 /*
 |--------------------------------------------------------------------------
