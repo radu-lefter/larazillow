7c7ada725529e09702e8995b1f191cff1fb42e2c
raduubraduu <lefter_radu@yahoo.com>
2023-05-02 21:58:43 +0100

    created backend logic for sorting

--- app/Http/Controllers/RealtorListingController.php
+++ app/Http/Controllers/RealtorListingController.php
@@ -20,7 +20,8 @@ class RealtorListingController extends Controller
     {
 
         $filters = [
-            'deleted' => $request->boolean('deleted')
+            'deleted' => $request->boolean('deleted'),
+            ...$request->only(['by', 'order'])
         ];
 
         return inertia(
@@ -28,7 +29,7 @@ class RealtorListingController extends Controller
             [
                 'listings' => Auth::user()
                     ->listings()
-                    ->mostRecent()
+                    //->mostRecent()
                     ->filter($filters)
                     ->get()
             ]
