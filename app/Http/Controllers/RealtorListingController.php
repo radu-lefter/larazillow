3f92a833d73941f3963697df2479e6ff2ffdc845
raduubraduu <lefter_radu@yahoo.com>
2023-05-02 22:38:46 +0100

    created func to restore listings

--- app/Http/Controllers/RealtorListingController.php
+++ app/Http/Controllers/RealtorListingController.php
@@ -110,4 +110,11 @@ class RealtorListingController extends Controller
         return redirect()->back()
             ->with('success', 'Listing was deleted!');
     }
+
+    public function restore(Listing $listing)
+    {
+        $listing->restore();
+
+        return redirect()->back()->with('success', 'Listing was restored!');
+    }
 }
