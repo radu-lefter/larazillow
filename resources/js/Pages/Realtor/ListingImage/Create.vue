be47ee2cc7ec8ca16e899df7c775b50b8cc7e16e
raduubraduu <lefter_radu@yahoo.com>
2023-05-03 23:09:15 +0100

    created validation for uploading images

--- resources/js/Pages/Realtor/ListingImage/Create.vue
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -14,24 +14,25 @@
                     Reset
                 </button>
             </section>
+            <div v-if="imageErrors.length" class="input-error">
+                <div v-for="(error, index) in imageErrors" :key="index">
+                    {{ error }}
+                </div>
+            </div>
         </form>
     </Box>
     <Box v-if="listing.images.length" class="mt-4">
-    <template #header>Current Listing Images</template>
-    <section class="mt-4 grid grid-cols-3 gap-4">
-      <div v-for="image in listing.images" :key="image.id" class="flex flex-col justify-between">
-        <img :src="image.src" class="rounded-md" />
-        <Link 
-          :href="route('realtor.listing.image.destroy', { listing: props.listing.id, image: image.id })"
-          method="delete"
-          as="button"
-          class="mt-2 btn-outline text-xs"
-        >
-          Delete
-        </Link>
-      </div>
-    </section>
-  </Box>
+        <template #header>Current Listing Images</template>
+        <section class="mt-4 grid grid-cols-3 gap-4">
+            <div v-for="image in listing.images" :key="image.id" class="flex flex-col justify-between">
+                <img :src="image.src" class="rounded-md" />
+                <Link :href="route('realtor.listing.image.destroy', { listing: props.listing.id, image: image.id })"
+                    method="delete" as="button" class="mt-2 btn-outline text-xs">
+                Delete
+                </Link>
+            </div>
+        </section>
+    </Box>
 </template>
   
 <script setup>
@@ -43,13 +44,14 @@ import NProgress from 'nprogress'
 
 const props = defineProps({ listing: Object })
 Inertia.on('progress', (event) => {
-  if (event.detail.progress.percentage) {
-    NProgress.set((event.detail.progress.percentage / 100) * 0.9)
-  }
+    if (event.detail.progress.percentage) {
+        NProgress.set((event.detail.progress.percentage / 100) * 0.9)
+    }
 })
 const form = useForm({
     images: [],
 })
+const imageErrors = computed(() => Object.values(form.errors))
 const canUpload = computed(() => form.images.length)
 const upload = () => {
     form.post(
