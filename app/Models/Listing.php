4ab7ff7e5e8612e90240bc28bb046228c5d7f98e
raduubraduu <lefter_radu@yahoo.com>
2023-05-03 22:25:27 +0100

    created model and migrations for listing image

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Listing extends Model
 {
@@ -26,6 +27,11 @@ class Listing extends Model
             \App\Models\User::class,
             'by_user_id'
         );
+    }
+
+    public function images(): HasMany
+    {
+        return $this->hasMany(ListingImage::class);
     }
 
     public function scopeMostRecent(Builder $query): Builder
