41454538a122f63e1a6c688bd30724505afe67c6
raduubraduu <lefter_radu@yahoo.com>
2023-05-04 18:35:12 +0100

    displayed numbers of offers made in realtor listing page

--- app/Http/Controllers/RealtorListingController.php
+++ app/Http/Controllers/RealtorListingController.php
@@ -33,9 +33,18 @@ class RealtorListingController extends Controller
                     //->mostRecent()
                     ->filter($filters)
                     ->withCount('images')
+                    ->withCount('offers')
                     ->paginate(5)
                     ->withQueryString()
             ]
+        );
+    }
+
+    public function show(Listing $listing)
+    {
+        return inertia(
+            'Realtor/Show',
+            ['listing' => $listing->load('offers')]
         );
     }
 
